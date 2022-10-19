f9a7194f6e6d3ccf917a3d0fcda50952f37b05b3
Bhi Wong <mochi@DESKTOP-JB6D21O.localdomain>
2022-10-20 01:14:57 +1100

    Authorization added

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from datetime import date, timedelta
 from flask_marshmallow import Marshmallow
 from flask_bcrypt import Bcrypt
 from sqlalchemy.exc import IntegrityError
-from flask_jwt_extended import JWTManager, create_access_token, jwt_required
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 
 app = Flask(__name__)
 app.config ['JSON_SORT_KEYS'] = False
@@ -44,6 +44,14 @@ class CardSchema(ma.Schema):
     class Meta:
         fields = ('id', 'title', 'description', 'status', 'priority', 'date')
         ordered = True
+
+
+
+def authorize():
+    user_id = get_jwt_identity()
+    stmt = db.select(User).filter_by(id=user_id)
+    user = db.session.scalar(stmt)
+    return user.is_admin
 
 
 # Define a custom CLI (terminal) command
@@ -127,6 +135,7 @@ def auth_register():
         return {'error': 'Email address already in use'}, 409
 
 
+
 @app.route('/auth/login/', methods=['POST'])
 def auth_login():
     # Find a user by email address
@@ -140,9 +149,13 @@ def auth_login():
     else:
         return {'error': 'Invalid email or password'}, 401
 
+
 @app.route('/cards/')
 @jwt_required()
 def all_cards():
+    if not authorize():
+        return {'error': 'You must be an admin'}, 401
+
     # Old Way Legacy
     # select * from cards;
     # cards = Card.query.all() <- Old way 
