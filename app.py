d7682503dce342306a9b57dfac4761ab5466807d
Bhi Wong <mochi@DESKTOP-JB6D21O.localdomain>
2022-10-19 11:17:47 +1100

    Revert "it broke"
    This reverts commit e37e671975451ed3875ef25e709b0b8d8ceece03.

--- app.py
+++ app.py
@@ -110,11 +110,13 @@ def seed_db():
 @app.route('/auth/register/', methods=['POST'])
 def auth_register():
     try:
+        # Load the posted user info and parse the JSON
+        user_info = UserSchema().load(request.json)
         # Create a new User model instance from the user_info
         user = User(
-            email = request.json['email'],
+            email = user_info['email'],
             password = bcrypt.generate_password_hash(user_info['password']).decode('utf8'),
-            name = request.json['name']
+            name = user_info['name']
         )
 
         # Add and commit user to DB
@@ -124,12 +126,6 @@ def auth_register():
         return UserSchema(exclude=['password']).dump(user), 201
     except IntegrityError:
         return {'error': 'Email address already in use'}, 409
-
-@app.route('/auth/login/', methods=['POST'])
-def auth_login():
-    stmt = db.select(User).filter_by(email=request.json['email'], password=bcrypt.generate_password_hash(user_info['password']).decode('utf8'))
-    user = db.session.scalar(stmt)
-    return UserSchema(exclude=['password']).dump(user), 201
 
 
 @app.route('/cards/')
