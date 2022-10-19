73d338f6b063adc43f1ea9bc2628c584d0184291
Bhi Wong <mochi@DESKTOP-JB6D21O.localdomain>
2022-10-19 11:23:44 +1100

    Recreated app.py

--- app.py
+++ app.py
@@ -1,47 +1,20 @@
-from flask import Flask, jsonify, request
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from datetime import date
-from flask_marshmallow import Marshmallow
-from flask_bcrypt import Bcrypt
-from sqlalchemy.exc import IntegrityError
 
 app = Flask(__name__)
-app.config ['JSON_SORT_KEYS'] = False
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://trello_dev:password123@127.0.0.1:5432/trello'
+app.config['SQLALCHEMY_DATABASE_URI'] ='postgresql+psycopg2://trello_dev:password123@127.0.0.1:5432/trello'
 
 db = SQLAlchemy(app)
-ma = Marshmallow(app)
-bcrypt = Bcrypt(app)
-
-class User(db.Model):
-    __tablename__ = 'users'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    email = db.Column(db.String, nullable=False, unique=True)
-    password = db.Column(db.String, nullable=False)
-    is_admin = db.Column(db.Boolean, default=False)
-
-class UserSchema(ma.Schema):
-    class Meta:
-        fields = ('id', 'name', 'email', 'password', 'is_admin')
-
-
 class Card(db.Model):
     __tablename__ = 'cards'
-
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
     description = db.Column(db.Text)
     date = db.Column(db.Date)
     status = db.Column(db.String)
     priority = db.Column(db.String)
-
-class CardSchema(ma.Schema):
-    class Meta:
-        fields = ('id', 'title', 'description', 'status', 'priority', 'date')
-        ordered = True
 
 
 # Define a custom CLI (terminal) command
@@ -50,6 +23,8 @@ def create_db():
     db.create_all()
     print("Tables created")
 
+
+
 @app.cli.command('drop')
 def drop_db():
     db.drop_all()
@@ -57,33 +32,20 @@ def drop_db():
 
 @app.cli.command('seed')
 def seed_db():
-    users = [
-        User(
-            email='admin@spam.com',
-            password=bcrypt.generate_password_hash('eggs').decode('utf-8'),
-            is_admin=True
-        ),
-        User(
-            name='John Cleese',
-            email='someone@spam.com',
-            password=bcrypt.generate_password_hash('12345').decode('utf-8')
-        )
-    ]
-
     cards = [
         Card(
             title = 'Start the project',
-            description = 'Stage 1 - Create the database',
+            description = 'Stage 1 - creating the database',
             status = 'To Do',
             priority = 'High',
             date = date.today()
         ),
         Card(
-            title = "SQLAlchemy",
-            description = "Stage 2 - Integrate ORM",
-            status = "Ongoing",
-            priority = "High",
-            date = date.today()
+        title = "SQLAlchemy",
+        description = "Stage 2 - Integrate ORM",
+        status = "Ongoing",
+        priority = "High",
+        date = date.today()
         ),
         Card(
             title = "ORM Queries",
@@ -100,66 +62,12 @@ def seed_db():
             date = date.today()
         )
     ]
-
+        
     db.session.add_all(cards)
-    db.session.add_all(users)
     db.session.commit()
     print('Tables seeded')
 
-
-@app.route('/auth/register/', methods=['POST'])
-def auth_register():
-    try:
-        # Create a new User model instance from the user_info
-        user = User(
-            email = request.json['email'],
-            password = bcrypt.generate_password_hash(user_info['password']).decode('utf8'),
-            name = request.json['name']
-        )
-
-        # Add and commit user to DB
-        db.session.add(user)
-        db.session.commit()
-        # Respond to client
-        return UserSchema(exclude=['password']).dump(user), 201
-    except IntegrityError:
-        return {'error': 'Email address already in use'}, 409
-
-@app.route('/auth/login/', methods=['POST'])
-def auth_login():
-    stmt = db.select(User).filter_by(email=request.json['email'], password=bcrypt.generate_password_hash(user_info['password']).decode('utf8'))
-    user = db.session.scalar(stmt)
-    return UserSchema(exclude=['password']).dump(user), 201
-
-
-@app.route('/cards/')
-def all_cards():
-    # select * from cards;
-    # cards = Card.query.all()
-    # stmt = db.select(Card).where(Card.status == 'To Do')
-    stmt = db.select(Card).order_by(Card.priority.desc(), Card.title)
-    cards = db.session.scalars(stmt)
-    return CardSchema(many=True).dump(cards)
-    # for card in cards:
-    #     print(card.title, card.priority)
-    # print(cards)
-    # print(cards[0].__dict__)
-
-@app.cli.command('first_card')
-def first_card():
-    # select * from cards limit 1;
-    # card = Card.query.first()
-    stmt = db.select(Card).limit(1)
-    card = db.session.scalar(stmt)
-    print(card.__dict__)
-
-@app.cli.command('count_ongoing')
-def count_ongoing():
-    stmt = db.select(db.func.count()).select_from(Card).filter_by(status='Ongoing')
-    cards = db.session.scalar(stmt)
-    print(cards)
-
-
+    
 @app.route('/')
 def index():
     return "Hello World!"
